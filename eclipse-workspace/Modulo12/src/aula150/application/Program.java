d8a20a84869d5dfb6f324449de56bb735b9317c4
Luiz Pinheiro <luizpinheiro.rs@gmail.com>
2018-08-26 11:54:18 -0300

    Finalizado Modulo 12

---
+++ eclipse-workspace/Modulo12/src/aula150/application/Program.java
@@ -0,0 +1,27 @@
+package aula150.application;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		File file = new File("c:\\temp\\in.txt");
+		Scanner sc = null;
+		try {
+			sc = new Scanner(file);
+			while (sc.hasNextLine()) {
+				System.out.println(sc.nextLine());
+			}
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			if(sc != null) {
+				sc.close();
+			}
+		}
+
+	}
+
+}
